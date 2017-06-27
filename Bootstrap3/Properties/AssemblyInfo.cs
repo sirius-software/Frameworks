bc025c4b27240da44745f6b7f1539733c63e628e
LeonidVeriga <leonid@object.net>
2017-06-28 00:46:13 +0300

    Update to -beta4

--- Bootstrap3/Properties/AssemblyInfo.cs
+++ Bootstrap3/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("ebd43cb4-1057-4dc5-9a7e-78c96c08b2c9")]
 [assembly: AssemblyVersion("3.9")]
 [assembly: AssemblyFileVersion("3.9.5")]
-[assembly: AssemblyInformationalVersion("3.9.5-beta3")]
+[assembly: AssemblyInformationalVersion("3.9.5-beta4")]
